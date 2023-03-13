8eae1f8ef52fe74b0e86159cf2d6ca3dcb3ff816
hdepoumps <65735967+hdepoumps@users.noreply.github.com>
2023-03-13 10:43:14 +0100

    Add files via upload

---
+++ controllers/sauces.js
@@ -0,0 +1,41 @@
+const Sauce = require("../models/Sauces");
+
+exports.createSauce = (req,res,next) => {
+
+    const sauceObject = JSON.parse(req.body.sauce);
+
+    delete sauceObject._id;
+    delete sauceObject.userId;
+    const sauce = new Sauce({
+        ...sauceObject,
+        userId: req.auth.userId,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    });
+    sauce.save()
+        .then(()=> res.status(201).json({ message:'Objet enregistré !'}))
+        .catch(error => res.status(400).json({error}));
+};
+
+exports.modifySauce = (req,res,next)=>{
+    Sauce.updateOne( {_id: req.params.id }, { ...req.body, _id: req.params.id })
+        .then(()=> res.status(200).json({message:'Objet modifié !'}))
+        .catch(error => res.status(400).json({error}));
+};
+
+exports.deleteSauce = (req, res, next) => {
+    Sauce.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+        .catch(error => res.status(400).json({ error }));
+};
+
+exports.getAllSauces =(req,res,next)=>{
+    Sauce.find()
+        .then(Sauces => res.status(200).json(Sauces))
+        .catch(error => res.status(400).json({error}));
+};
+
+exports.getOneSauce = (req, res, next) => {
+    Sauce.findOne({_id: req.params.id})
+        .then(sauce =>res.status(200).json(sauce))
+        .catch(error =>res.status(404).json({error}));
+};
